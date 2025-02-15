5dc1d8388a69d4ef421d8c54ed7e17a05af2d369
김영민 <youngmin0807ray@gmail.com>
2025-02-15 09:45:30 +0900

    [chore] prettier 설정

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+// @ts-check
+/** @type {import("prettier").Config} */
+const config = {
+  semi: true,
+  singleQuote: true,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
